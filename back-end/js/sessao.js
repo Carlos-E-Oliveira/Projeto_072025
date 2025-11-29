a0bed0b0bdb791db0ea2df46c0fb411fc24c0083
Carlos-E-Oliveira <carlos.eduardo.o@uni9.edu.br>
2025-11-28 22:13:46 -0300

    Correção do arquivo sessao.js

--- back-end/js/sessao.js
+++ back-end/js/sessao.js
@@ -1,3 +1,10 @@
+function mostrarSecao(secaoId) {
+    const secoes = document.querySelectorAll('.secao');
+    secoes.forEach((secao) => secao.classList.remove('ativa'));
+
+    document.getElementById(secaoId).classList.add('ativa');
+}
+
 async function sair() {
     const req = await fetch("../../back-end/php/logout.php");
     const resp = await req.text();
